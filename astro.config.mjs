97d02826fa6195e53043821a7b29627b54156deb
charusaie <amin_ch84@yahoo.com>
2024-02-16 16:21:54 +0100

    change config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://charusaie.github.io',
+  base: '/charusaie.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
